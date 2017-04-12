6a920aace4dcd5a2c7c90ce872da074b9893e3d2
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2017-04-12 19:25:34 +0000

    beautify

--- ruuvi-scanner/ruuvi-scanner.js
+++ ruuvi-scanner/ruuvi-scanner.js
@@ -4,27 +4,27 @@ const util = require('util');
 const url = require('url');
 
 
-var RuuviScanner = function() {
-   EventEmitter.call(this);
-   scanner.on('found', this.beaconFound.bind(this));
-   scanner.on('updated', this.beaconFound.bind(this));
+var RuuviScanner = function () {
+    EventEmitter.call(this);
+    scanner.on('found', this.beaconFound.bind(this));
+    scanner.on('updated', this.beaconFound.bind(this));
 }
 
 util.inherits(RuuviScanner, EventEmitter);
 
-RuuviScanner.prototype.scan = function() {
+RuuviScanner.prototype.scan = function () {
     scanner.startScanning(true);
     setTimeout(() => {
         scanner.stopScanning();
     }, 5000);
 }
 
-RuuviScanner.prototype.start = function(scannerConfig) {
+RuuviScanner.prototype.start = function (scannerConfig) {
     setInterval(this.scan.bind(this), scannerConfig.interval);
     this.scan();
 }
 
-RuuviScanner.prototype.parseData = function(beacon) {
+RuuviScanner.prototype.parseData = function (beacon) {
 
     const now = Math.floor(Date.now() / 1000);
     const hash = url.parse(beacon.url).hash.substring(1);
@@ -32,21 +32,21 @@ RuuviScanner.prototype.parseData = function(beacon) {
 
     /* Check the format.. only base64 supported nowadays */
     if (decoded[0] == 2) {
-     const uTemp = ((decoded[2] & 127) << 8) | decoded[3];
-     const tempSign = (decoded[2] >> 7) & 1;
-        
-     const data = {
- 	beacon : beacon.id,
-	timestamp : now,
-        temperature : tempSign === 0 ? uTemp/256.0 : -1 * uTemp/256.0,
-        pressure : (((decoded[4] << 8) + decoded[5]) + 50000)/100,
-        humidity : decoded[1] * 0.5
-     };
-     this.emit('data', data); 
+        const uTemp = ((decoded[2] & 127) << 8) | decoded[3];
+        const tempSign = (decoded[2] >> 7) & 1;
+
+        const data = {
+            beacon: beacon.id,
+            timestamp: now,
+            temperature: tempSign === 0 ? uTemp / 256.0 : -1 * uTemp / 256.0,
+            pressure: (((decoded[4] << 8) + decoded[5]) + 50000) / 100,
+            humidity: decoded[1] * 0.5
+        };
+        this.emit('data', data);
     }
 }
 
-RuuviScanner.prototype.beaconFound = function(beacon) {
+RuuviScanner.prototype.beaconFound = function (beacon) {
     if (beacon.url && beacon.url.indexOf('ruu.vi') >= 0) {
         this.parseData(beacon);
     }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const scanner = require('./ruuvi-scanner');
 const firebaseConfig = config.get('firebase');
 const scannerConfig = config.get('scanner');
 const ruuvitags = config.get('ruuvitags');
- 
+
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
@@ -14,58 +14,52 @@ const database = firebase.database();
 let lastTimes = new Map();
 
 
-scanner.on('data', function(data) {
+scanner.on('data', function (data) {
 
-if (lastTimes.has(data.beacon))
-{
-  if (data.timestamp < (lastTimes.get(data.beacon) + (scannerConfig.interval / 1000)))
-  {
-     return;
+  if (lastTimes.has(data.beacon)) {
+    if (data.timestamp < (lastTimes.get(data.beacon) + (scannerConfig.interval / 1000))) {
+      return;
+    }
   }
-}
-lastTimes.set(data.beacon, data.timestamp);
+  lastTimes.set(data.beacon, data.timestamp);
 
-if (!ruuvitags.hasOwnProperty(data.beacon))
-{   
- console.log("data from unknown beacon %s, maybe add it to config?", data.beacon);
- return;
-}
-
-
-
- const date = moment(data.timestamp*1000).utc().format('YYYY-MM-DD');
-
- var alias;
- var hasAlias = false;
- 
- if (!ruuvitags[data.beacon].alias)
- {
-   alias = "unknown";
- }
- else
- {
-  alias = ruuvitags[data.beacon].alias;
-  hasAlias = true;
+  if (!ruuvitags.hasOwnProperty(data.beacon)) {
+    console.log("data from unknown beacon %s, maybe add it to config?", data.beacon);
+    return;
   }
 
- console.log("%s (%d) beacon=%s (%s), t=%s C, P=%s hPa, r=%s %", moment(data.timestamp * 1000).format('YYYY-MM-DD h:mm:ss'), data.timestamp, alias, data.beacon, data.temperature, data.pressure, data.humidity);
+  const date = moment(data.timestamp * 1000).utc().format('YYYY-MM-DD');
 
-/* Save to list of devices the configured alias. */
- if (hasAlias)
-{ 
-database.ref().child('devices').child(data.beacon).set({
-	alias : ruuvitags[data.beacon].alias
-	});
-}
-/* Save the measurement result*/
- const measurementRef = database.ref().child('measurements').child(data.beacon).push();
- 
- measurementRef.set({
-	    temperature: data.temperature,
-	    pressure: data.pressure,
-            humidity: data.humidity,
-	    timestamp: data.timestamp
- });
+  var alias;
+  var hasAlias = false;
+
+  if (!ruuvitags[data.beacon].alias) {
+    alias = "unknown";
+  } else {
+    alias = ruuvitags[data.beacon].alias;
+    hasAlias = true;
+  }
+
+  console.log("%s (%d) beacon=%s (%s), t=%s C, P=%s hPa, r=%s %",
+    moment(data.timestamp * 1000).format('YYYY-MM-DD h:mm:ss'), 
+    data.timestamp, alias, data.beacon, data.temperature, 
+    data.pressure, data.humidity);
+
+  /* Save to list of devices the configured alias. */
+  if (hasAlias) {
+    database.ref().child('devices').child(data.beacon).set({
+      alias: ruuvitags[data.beacon].alias
+    });
+  }
+  /* Save the measurement result*/
+  const measurementRef = database.ref().child('measurements').child(data.beacon).push();
+
+  measurementRef.set({
+    temperature: data.temperature,
+    pressure: data.pressure,
+    humidity: data.humidity,
+    timestamp: data.timestamp
+  });
 });
 
 console.log("starting scanning with interval %d", scannerConfig.interval);
